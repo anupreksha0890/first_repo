8ff67902b50c3cbd6d9d5c7e87f4d0ebc39696d8
anupreksha0890 <anupreksha0890@gmail.com>
2025-12-10 15:22:39 +0530

    Update JenkinApplication.java

--- src/main/java/com/jenkins/jenkin/JenkinApplication.java
+++ src/main/java/com/jenkins/jenkin/JenkinApplication.java
@@ -2,10 +2,10 @@ package com.jenkins.jenkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinApplication {
-
+public class JenkinApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinApplication.class, args);
 	}
